cfa4d091a371bdac17bf52dceff6fde8058bd845
Neilk24 <neilk2050@gmail.com>
2024-09-16 19:42:46 -0400

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,80 @@
+j = input("Enter any lowercase alphabet character: ")
+
+if (j=="a"):
+    print(65)
+
+elif (j=="b"):
+    print(66)
+
+elif (j=="c"):
+    print(67)
+
+elif (j=="d"):
+    print(68)
+
+elif (j=="e"):
+    print(69)
+
+elif (j=="f"):
+    print(70)
+
+elif (j=="g"):
+    print(71)
+
+elif (j=="h"):
+    print(72)
+
+elif (j=="i"):
+    print(73)
+
+elif (j=="j"):
+    print(74)
+
+elif (j=="k"):
+    print(75)
+
+elif (j=="l"):
+    print(76)
+
+elif (j=="m"):
+    print(77)
+
+elif (j=="n"):
+    print(78)
+
+elif (j=="o"):
+    print(79)
+
+elif (j=="p"):
+    print(80)
+
+elif (j=="q"):
+    print(81)
+
+elif (j=="r"):
+    print(82)
+
+elif (j=="s"):
+    print(83)
+
+elif (j=="t"):
+    print(84)
+
+elif (j=="u"):
+    print(85)
+
+elif (j=="v"):
+    print(86)
+
+elif (j=="w"):
+    print(87)
+
+elif (j=="x"):
+    print(88)
+
+elif (j=="y"):
+    print(89)
+
+elif (j=="z"):
+    print(90)
+
